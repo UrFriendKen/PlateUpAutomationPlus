f128d93acedc0f34aabebb426901dc45c60d3b61
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-03-17 15:10:35 +0800

    Switch variable providers during night

---
+++ SwitchVariableProviderDuringNight.cs
@@ -0,0 +1,14 @@
+﻿using Kitchen;
+using Unity.Entities;
+
+namespace KitchenAutomationPlus
+{
+    [UpdateInGroup(typeof(InteractionGroup))]
+    [UpdateBefore(typeof(ShowPingedApplianceInfo))]
+    public class SwitchVariableProviderDuringNight : SwitchVariableProvider
+    {
+        protected override InteractionMode RequiredMode => InteractionMode.Appliances;
+
+        protected override InteractionType RequiredType => InteractionType.Notify;
+    }
+}
